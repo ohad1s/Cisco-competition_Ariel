820d29b1ad3a231dee3427cd52f288a3ff5b7f8d
shirazi97 <shirazi1997@gmail.com>
2022-12-22 10:30:55 +0200

    d_ex

--- data_exploration.py
+++ data_exploration.py
@@ -11,17 +11,17 @@ with open(f'combined_datasets_for_students/dataset_{str(dataset_number)}_train.j
     raw_ds = json.load(file)
 df = pd.json_normalize(raw_ds, max_level=2)
 
-# print(tabulate(df.head(), headers = 'keys', tablefmt = 'psql'))
+print(tabulate(df.head(), headers = 'keys', tablefmt = 'psql'))
 
 # sns.histplot(df['response.headers.Content-Length'])
 # sns.histplot(df['response.status_code'])
 # plt.show()
 
 # string_column = df['request.headers.Accept-Encoding'] # good feature to check!!
-string_column = df['request.headers.Sec-Fetch-User']
+# string_column = df['request.headers.Sec-Fetch-User']
 
 # Count the number of times each unique value appears in the column
-value_counts = string_column.value_counts(normalize=True)
+# value_counts = string_column.value_counts(normalize=True)
 
 # print(value_counts)
 # value_counts.plot(kind='bar')
